88bc7cd19d38e849c1640b35b1f7dd2b15e61ee9
Miro <mradwan@plawniowice.pl>
2018-12-28 22:05:16 +0100

    Init

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   int a = 12;
+            int b = 0;
+
+        try
+        {
+            a = a / b;
+        }
+        catch ( System.DivideByZeroException e) {
+            Console.WriteLine(e.Source);
+            Console.WriteLine("Wystąpił błąd dzielenia przez zero! Kończę działanie programu");
+            return;
+        }
+        catch (System.Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return;
+        }
+
+
+            Osoba miki  = new Osoba();
+            miki.setImie("Mikołaj");
+            miki.setNazwisko("Radwański");
+
+            Osoba tata = new Osoba();
+            tata.setImie("Mirek");
+
+            Console.WriteLine(miki.getImie() + ' ' + miki.getNazwisko() + ' ' + miki.getDataUr() );
+
+            Console.WriteLine(tata.getImie() + ' ' + tata.getNazwisko() + ' ' + tata.getDataUr() );
+        }
+    }
+}
